14b2663f4d3e2d02c4b5ea5c7891a8d9a7e660bc
Moose Davis <moosedavis2011@gmail.com>
2020-09-21 20:50:06 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,33 @@
 class Author
   
-  attr_accessor :post 
+  attr_accessor :name
   
-end 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    Post.all.select {|post| post.artist == self}
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_name(name)
+    post = Post.new(name)
+    post.artist = self
+  end
+  
+  def self.song_count
+    Post.all.count
+  end
+  
+end
